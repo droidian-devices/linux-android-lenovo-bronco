f5d59409a48f353507e0e235bb3a6cec275de1a1
John Galt <johngaltfirstrun@gmail.com>
2025-10-29 14:10:54 -0400

    evdi_fb: only kernel read memfd if gralloc buf id extraction fails
    Also fix overlooked leak in error path.

--- drivers/lindroid-drm/evdi_fb.c
+++ drivers/lindroid-drm/evdi_fb.c
@@ -195,14 +195,16 @@ struct drm_framebuffer *evdi_fb_user_fb_create(struct drm_device *dev,
 	efb->obj = bo;
 	efb->owner = file;
 	efb->active = true;
-	memfd_file = fget(mode_cmd->handles[0]);
-	if (memfd_file) {
-		bytes_read = kernel_read(memfd_file, &id, sizeof(id), &pos);
-		if (bytes_read == sizeof(id))
-			efb->gralloc_buf_id = id;
+	efb->gralloc_buf_id = evdi_fb_extract_gralloc_id(mode_cmd);
+	if (!efb->gralloc_buf_id) {
+		memfd_file = fget(mode_cmd->handles[0]);
+		if (memfd_file) {
+			bytes_read = kernel_read(memfd_file, &id, sizeof(id), &pos);
+			if (bytes_read == sizeof(id))
+				efb->gralloc_buf_id = id;
+			fput(memfd_file);
+		}
 	}
-	if (!efb->gralloc_buf_id)
-		efb->gralloc_buf_id = evdi_fb_extract_gralloc_id(mode_cmd);
 
 	ret = evdi_fb_init_core(dev, efb, mode_cmd);
 	if (ret) {
