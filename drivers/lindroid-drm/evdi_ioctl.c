85d52148e652f96bdae51d196fceb5fcfda9dc6f
arpio23 <alex@familyrutz.com>
2025-12-18 18:21:43 -0700

    lindroid-drm/evdi_ioctl: fix C90 warning by moving variable declarations to top of block
    Fixes build error when compiling with -Werror

--- drivers/lindroid-drm/evdi_ioctl.c
+++ drivers/lindroid-drm/evdi_ioctl.c
@@ -196,6 +196,7 @@ static inline struct evdi_inflight_req *evdi_inflight_alloc(struct evdi_device *
 #ifdef EVDI_HAVE_XARRAY
 	{
 		u32 xid;
+		u32 start_id;
 		int ret;
 #ifdef EVDI_HAVE_XA_ALLOC_CYCLIC
 		xid = READ_ONCE(evdi->inflight_next_id);
@@ -224,7 +225,7 @@ static inline struct evdi_inflight_req *evdi_inflight_alloc(struct evdi_device *
 		id = (int)xid;
 #else
 		xid = 0;
-		u32 start_id = READ_ONCE(evdi->inflight_next_id);
+		start_id = READ_ONCE(evdi->inflight_next_id);
 		if (unlikely(!start_id))
 			start_id = 1;
 		ret = xa_alloc(&evdi->inflight_xa, &xid, req,
